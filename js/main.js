86ef840166530ea096557e99b6323a82d09c8d9c
Shane <6071159+smashedr@users.noreply.github.com>
2023-08-30 14:54:35 -0700

    Testing

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 $(document).ready(function () {
 
-    // Open Links in New Window
-    $('a').each(function() {
-        if (!this.href.includes('index.html')) {
-            $(this).click(function (event) {
-                event.preventDefault();
-                event.stopPropagation();
-                window.open(this.href, '_blank');
-            });
-        }
-    });
+    // // Open Links in New Window
+    // $('a').each(function() {
+    //     $(this).click(function (event) {
+    //         event.preventDefault();
+    //         event.stopPropagation();
+    //         window.open(this.href, '_blank');
+    //     });
+    // });
 
 })
