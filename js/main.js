0e340d521c801b497773ed8d2f12e0c2ee30fce7
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2024-04-26 14:00:57 -0700

    Update Template (#3)
    * Update Template
    
    * Update Template Add Copy Button

--- js/main.js
+++ js/main.js
@@ -1,12 +1,1 @@
-// $(document).ready(function () {
-//
-//     // // Open Links in New Window
-//     // $('a').each(function() {
-//     //     $(this).click(function (event) {
-//     //         event.preventDefault();
-//     //         event.stopPropagation();
-//     //         window.open(this.href, '_blank');
-//     //     });
-//     // });
-//
-// })
+new ClipboardJS('.clip')
